4bc41ff832936a19d0d62eaec858d18bb8272605
JulieValdez <juliegragg0605@gmail.com>
2020-06-01 09:32:57 -0700

    folders and files all set up with initial data

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chipotle Turkey', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Wagu Dream', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fresno Fig', false);
